7630abafb5c9569894f3867b3b58cfa1d2a7eaf7
lindestad <99703478+lindestad@users.noreply.github.com>
2024-03-08 17:37:00 +0100

    refactor js -> further work on regex

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const operatorList = ['+', '-', '*', '/', ' mod '];
-function operatorInString(expression) {
-    return operatorList.some((e) => expression.includes(e));
-}
+// function operatorInString(expression) {
+//     return operatorList.some((e) => expression.includes(e));
+// }
 
 function evaluateExpression(expression) {
+    const regex = regexFromOperatorList(operatorList);
+    let s = expression.split(regexFromOperatorList(operatorList));
     let result;
     while (operatorInString(expression)) {
         // modulo
@@ -12,3 +14,26 @@ function evaluateExpression(expression) {
     // order of operations
     
 }
+
+function regexFromOperatorList(operatorList) {
+    let regexStr = '';
+    const reservedCharacters = '\\^$.|?*+';
+    for (const operator of operatorList) {
+        if (reservedCharacters.includes(operator)) {
+            regexStr += '\\' + operator + '|';
+        } else {
+            regexStr += operator + '|';
+        }
+    }
+    return new RegExp(regexStr.slice(0, -1));
+}
+
+let testStr = '12345+455/22*18+19 mod 32*9';
+
+
+
+const regex = regexFromOperatorList(operatorList);
+console.log(regex);
+
+console.log(testStr.split(regex));
+console.log(testStr.split(/\d/g).filter(n => n));
