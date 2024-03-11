6f8de388cae2698e81c5f39cad0234105e4bc8e0
lindestad <99703478+lindestad@users.noreply.github.com>
2024-03-11 11:54:10 +0100

    Abandon regex approach - replace with Object that tracks state.

--- script.js
+++ script.js
@@ -1,39 +1,7 @@
-const operatorList = ['+', '-', '*', '/', ' mod '];
-// function operatorInString(expression) {
-//     return operatorList.some((e) => expression.includes(e));
-// }
-
-function evaluateExpression(expression) {
-    const regex = regexFromOperatorList(operatorList);
-    let s = expression.split(regexFromOperatorList(operatorList));
-    let result;
-    while (operatorInString(expression)) {
-        // modulo
-    }
-    if (expression === '') {result = ''};
-    // order of operations
-    
-}
-
-function regexFromOperatorList(operatorList) {
-    let regexStr = '';
-    const reservedCharacters = '\\^$.|?*+';
-    for (const operator of operatorList) {
-        if (reservedCharacters.includes(operator)) {
-            regexStr += '\\' + operator + '|';
-        } else {
-            regexStr += operator + '|';
-        }
-    }
-    return new RegExp(regexStr.slice(0, -1));
-}
-
-let testStr = '12345+455/22*18+19 mod 32*9';
-
-
-
-const regex = regexFromOperatorList(operatorList);
-console.log(regex);
-
-console.log(testStr.split(regex));
-console.log(testStr.split(/\d/g).filter(n => n));
+const calculatorState = {
+    currentValue: '0',
+    previousValue: null,
+    currentOperation: null,
+    awaitingNewInput: true,
+    operationString: '',
+};
